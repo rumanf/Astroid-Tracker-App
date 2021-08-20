8c6b7406ee478a05a280246ed64b2db14b836977
rumanf <78289389+rumanf@users.noreply.github.com>
2021-08-20 15:21:43 -0400

    API key removed again after more testing

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     //API KEY REMOVED
-    //const val API_KEY = "DEMO_KEY"
-    const val API_KEY = "FX1dnvbjT9ak9Aig57JLmcPHmDxcUSGkbRT0BeGC"
+    const val API_KEY = "DEMO_KEY"
+
 
 }
